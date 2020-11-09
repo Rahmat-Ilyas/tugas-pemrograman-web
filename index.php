e50cee4208ee9bd75f6abf2c75d56c4e3c0522ed
Rahmat Ilyas <63534535+Rahmat-Ilyas@users.noreply.github.com>
2020-11-09 22:58:59 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,73 +1,116 @@
+<?php 
+$conn = mysqli_connect("localhost", "root", "", "db_tugas");
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Tugas 2 Pemrograman Web</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> -->
 </head>
 <body>
 	<div class="container">
-		<h1>Biodata</h1>
-		<form method="post">
+		<div class="row">
+			<h1>Biodata</h1>
+			<form method="post">
 
-			<label>Nama</label>
-			<input type="text" name="nama" placeholder="Masukkan Nama.." required>
+				<label>Nama</label>
+				<input type="text" name="nama" placeholder="Masukkan Nama.." required>
 
-			<label>NIM</label>
-			<input type="number" name="nim" placeholder="Masukkan Nim.." required>
+				<label>NIM</label>
+				<input type="number" name="nim" placeholder="Masukkan Nim.." required>
 
-			<label>Fakultas</label>
-			<select name="fakultas">
-				<option>Saintek</option>
-				<option>Febi</option>
-				<option>Syariah</option>
-				<option>Ushuluddin</option>
-				<option>Tarbiyah</option>
-				<option>Dakwah</option>
-				<option>Kesehatan</option>
-			</select>
-			
-			<label>Program Studi</label>
-			<select name="jurusan">
-				<option>Sistem Informasi</option>
-				<option>Teknik Informatika</option>
-				<option>Arsitektur</option>
-				<option>Peternakan</option>
-				<option>Matematika</option>
-				<option>Kimia</option>
-				<option>Biologi</option>
-				<option>Fisika</option>
-				<option>PWK</option>
-			</select>
+				<label>Fakultas</label>
+				<select name="fakultas">
+					<option>Saintek</option>
+					<option>Febi</option>
+					<option>Syariah</option>
+					<option>Ushuluddin</option>
+					<option>Tarbiyah</option>
+					<option>Dakwah</option>
+					<option>Kesehatan</option>
+				</select>
+				
+				<label>Program Studi</label>
+				<select name="jurusan">
+					<option>Sistem Informasi</option>
+					<option>Teknik Informatika</option>
+					<option>Arsitektur</option>
+					<option>Peternakan</option>
+					<option>Matematika</option>
+					<option>Kimia</option>
+					<option>Biologi</option>
+					<option>Fisika</option>
+					<option>PWK</option>
+				</select>
 
-			<label>Status Akademik</label>
-			<div id="radio">
-				<input id="aktif" name="status" type="radio" value="Aktif" checked>
-				<label for="aktif">Aktif</label>
-				<input id="lulus" name="status" type="radio" value="Lulus">
-				<label for="lulus">Lulus</label>
-				<input id="nonaktif" name="status" type="radio" value="Non Aktif">
-				<label for="nonaktif">Non Aktif</label>
-				<input id="dropout" name="status" type="radio" value="Dropout">
-				<label for="dropout">Dropout</label>
-			</div>
+				<label>Status Akademik</label>
+				<div id="radio">
+					<input id="aktif" name="status" type="radio" value="Aktif" checked>
+					<label for="aktif">Aktif</label>
+					<input id="lulus" name="status" type="radio" value="Lulus">
+					<label for="lulus">Lulus</label>
+					<input id="nonaktif" name="status" type="radio" value="Non Aktif">
+					<label for="nonaktif">Non Aktif</label>
+					<input id="dropout" name="status" type="radio" value="Dropout">
+					<label for="dropout">Dropout</label>
+				</div>
 
-			<button type="submit" name="submit" value="submit">Submit</button>
-			<input type="reset" value="Reset">
+				<button type="submit" name="submit" value="submit">Submit</button>
+				<input type="reset" value="Reset">
 
-		</form>
+			</form>
+		</div>
 
-<?php if (isset($_POST['submit'])) { ?>
-<div>
-<h1>Tampilan Data Inputan</h1>
-<hr>
-<span>Nama: <?= $_POST['nama'] ?></span><br>
-<span>NIM: <?= $_POST['nim'] ?></span><br>
-<span>Fakultas: <?= $_POST['fakultas'] ?></span><br>
-<span>Program Studi: <?= $_POST['jurusan'] ?></span><br>
-<span>Status Akademin: <?= $_POST['status'] ?></span><br>
-</div>
-<?php } ?>
-
+		<?php if (isset($_POST['submit'])) {
+		$nama = $_POST['nama'];
+		$nim = $_POST['nim'];
+		$fakultas = $_POST['fakultas'];
+		$jurusan = $_POST['jurusan'];
+		$status = $_POST['status'];
+		?>
+		<div>
+			<h1>Data Berhasil Diinputan</h1>
+			<hr>
+			<span>Nama: <?= $nama ?></span><br>
+			<span>NIM: <?= $nim ?></span><br>
+			<span>Fakultas: <?= $fakultas ?></span><br>
+			<span>Program Studi: <?= $jurusan ?></span><br>
+			<span>Status Akademin: <?= $status ?></span><br>
+		</div>
+		<?php 
+		mysqli_query($conn, "INSERT INTO tb_mahasiswa VALUES (NULL, '$nama', '$nim', '$fakultas', '$jurusan', '$status')");
+		} ?>
+		<br>
+		<h1>Data Mahasiswa</h1>
+		<hr>
+		<table class="table">
+		  <thead>
+		    <tr>
+		      <th scope="col">#</th>
+		      <th scope="col">Nama</th>
+		      <th scope="col">NIM</th>
+		      <th scope="col">Fakultas</th>
+		      <th scope="col">Program Studi</th>
+		      <th scope="col">Status Akademik</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		  	<?php 
+		  	$result = mysqli_query($conn, "SELECT * FROM tb_mahasiswa");
+		  	$no = 1;
+		  	foreach ($result as $dta) { ?>
+		    <tr>
+		      <th scope="row"><?= $no ?></th>
+		      <td><?= $dta["nama"] ?></td>
+		      <td><?= $dta["nim"] ?></td>
+		      <td><?= $dta["fakultas"] ?></td>
+		      <td><?= $dta["jurusan"] ?></td>
+		      <td><?= $dta["status"] ?></td>
+		    </tr>
+			<?php $no = $no +1; } ?>
+		  </tbody>
+		</table>
 	</div>
 </body>
 <style type="text/css">
